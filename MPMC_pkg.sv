4492e922ff6c40bdb1e47229fadcabcd6de880d4
Tarek Rayan <tarekrayyan50@gmail.com>
2025-01-10 02:24:38 +0200

    Multi-Port Memory Controller Package added

--- MPMC_pkg.sv
+++ MPMC_pkg.sv
@@ -1,11 +1,12 @@
 package MPMC_pkg;
     import uvm_pkg::*;
     `include "uvm_macros.svh"
-
     `include "sequence_item.svh"
-    // `include "random_sequence.svh"    
-    //`include "write_sequence.svh"
-    //`include "read_sequence.svh"    
+    `include "base_sequence.svh"
+    `include "Cpu1WriteSequence.svh"    
+    `include "Cpu1ReadSequence.svh"
+    `include "Cpu2WriteSequence.svh"    
+    `include "Cpu2ReadSequence.svh"
     `include "sequencer.svh"
     `include "driver.svh"    
     `include "monitor.svh"   
@@ -13,6 +14,8 @@ package MPMC_pkg;
     `include "scoreboard.svh"   
     `include "coverage.svh"     
     `include "env.svh"   
-    //`include "SerialSeq.svh"
-    //`include "full_test.svh" 
+    `include "base_test.svh"
+    `include "cpu1_test.svh" 
+    `include "cpu2_test.svh"
+    `include "cpu1_cpu2_test.svh"
 endpackage :MPMC_pkg
