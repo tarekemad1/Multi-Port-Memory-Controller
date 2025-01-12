56bf21dbdcbf02ed4d3f98c6b7b7fe4b4e187d92
Tarek Rayan <tarekrayyan50@gmail.com>
2025-01-12 16:19:45 +0200

    Read Sequence for Cpu 1

--- tb_classes/sequences/Cpu1ReadSequence.svh
+++ tb_classes/sequences/Cpu1ReadSequence.svh
@@ -6,11 +6,13 @@ class Cpu1ReadSeq extends base_sequence;
         super.new(name);
     endfunction
     task body();
+    repeat(5) begin 
         item=sequence_item::type_id::create("item");
         start_item(item);
-        assert(item.randomize() with{req_1==ACTIVE;rw_1==DEACTIVE;})
+        assert(item.randomize() with{req_1==ACTIVE;req_2==DEACTIVE;rw_1==DEACTIVE;})
             else `uvm_fatal("CPU1_READ_SEQ","Error in Asserting read");
         finish_item(item);
+    end 
     endtask
 endclass
    	
