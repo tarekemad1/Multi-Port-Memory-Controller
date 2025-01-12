a68f2e7194dbe8f77fa4a0eec35866f3113b72ba
Tarek Rayan <tarekrayyan50@gmail.com>
2025-01-12 16:20:17 +0200

    Write Sequence for Cpu2

--- tb_classes/sequences/Cpu1WriteSequence.svh
+++ tb_classes/sequences/Cpu1WriteSequence.svh
@@ -4,10 +4,13 @@ class Cpu1WriteSeq extends base_sequence;
         super.new(name);
     endfunction
     task body();
+    repeat(10)begin 
         item=sequence_item::type_id::create("item");
         start_item(item);
-            Randomization: assert (item.randomize() with {req_1==ACTIVE;rw_1==ACTIVE;})
+            Randomization: assert (item.randomize() with {req_1==ACTIVE ; req_2==DEACTIVE ;rw_1==ACTIVE;})
                 else `uvm_fatal("CPU1_WRITE_SEQ","Assertion Randomization failed!");
         finish_item(item);
+    end 
     endtask
+    
 endclass
