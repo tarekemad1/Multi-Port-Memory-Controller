d71abbc37a63860decca66acf89f0311809348f5
Tarek Rayan <tarekrayyan50@gmail.com>
2025-01-23 00:03:40 +0200

    Top level module

--- top.sv
+++ top.sv
@@ -3,10 +3,10 @@ module top;
     import MPMC_pkg::*;
     parameter HALF_CYCLE =5 ; 
     parameter rst_n_TIME  =20 ; 
-    logic rst_n , clk ; 
+    logic  clk ; 
 
-    IF vif (clk , rst_n );
-    MultiPortMemoryController   dut(clk,rst_n, vif.req_1 , vif.req_2 , vif.rw_1, vif.rw_2 , 
+    IF vif (clk );
+    MultiPortMemoryController   dut(clk,vif.rst_n, vif.req_1 , vif.req_2 , vif.rw_1, vif.rw_2 , 
                     vif.addr_1 , vif.addr_2,vif.data_in_1,vif.data_in_2,vif.grant_1,vif.grant_2
                     ,vif.data_out_1,vif.data_out_2,vif.mem_addr,vif.mem_rw,vif.mem_data_in,vif.mem_data_out);
 
@@ -18,12 +18,12 @@ module top;
         end
     end
 
-    initial begin
+    /*initial begin
         rst_n = 1;
         #rst_n_TIME ;
         rst_n=0;
         #20 rst_n = 1; // Keep reset asserted for 2 clock cycles
-    end
+    end*/
 
     initial begin
         uvm_config_db #(virtual IF)::set(null, "*", "vif", vif);
